c5c7486e9028ac8cffae1f6020035ff34405676f
DedierJr <dedieraguiar@gmail.com>
2023-09-06 07:09:51 -0300

    penúltima aula

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,21 @@
 import {Container} from "./styles"
 import  reminderLogo from "../../assets/images/Logo.jpg"
+import { Button } from "../Button";
+import { useState } from "react";
 
 export function Header() {
+    const [number, setNumber] = useState(0)
+    
+    function increment() {
+        setNumber(number + 1);
+    }
     return( 
     <Container>
         <img src={reminderLogo} alt="Logo React Reminder" />
-        <button>Adicionar Lembrete</button>
+
+        {/* <p style={{ color: "white" }}>{number}</p> */}
+
+        <Button title="Adicionar Lembrete" onClick={increment}/>
     </Container>
     );
 }
